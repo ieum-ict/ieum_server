27d662e1f525dc344858cc36590e6670cd84d82f
shawnatak926 <poomepoo90@gmail.com>
2026-08-24 16:13:09 +0900

    test : PrestgreSQL Testcontainers 기반 테스트 환경 구성

--- src/test/java/com/ieum/ict/ieum/IeumApplicationTests.java
+++ src/test/java/com/ieum/ict/ieum/IeumApplicationTests.java
@@ -1,10 +1,19 @@
 package com.ieum.ict.ieum;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class IeumApplicationTests {
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16-alpine");
+
     @Test
     void contextLoads() {
     }
